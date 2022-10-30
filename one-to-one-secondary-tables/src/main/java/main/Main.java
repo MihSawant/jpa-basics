0d4fb9768228db4c37daec8a4320d0ac0bd3db3b
Mihir Sawant <mihirssawant06@gmail.com>
2022-10-30 10:45:22 +0530

    added one-to-one using secondary tables method

--- one-to-one-secondary-tables/src/main/java/main/Main.java
+++ one-to-one-secondary-tables/src/main/java/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
+import entities.Student;
 import jakarta.persistence.Persistence;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +13,23 @@ public class Main {
         try{
             em.getTransaction().begin();
 
+            Student s1 = new Student();
+            s1.setName("Mihir");
+            s1.setCourseName("MCA");
+
+
+           Student s2 = new Student();
+           s2.setName("Hari");
+           s2.setCourseName("MCA");
+
+           Student s3 = new Student();
+           s3.setName("Soumesh");
+           s3.setCourseName("MBA-TECH");
+
+            List.of(s2, s3).forEach(em::persist);
 
             em.getTransaction().commit();
+            em.close();
         }catch (Exception ex){
             System.out.println("Something Went wrong "+ex.getMessage());
             em.getTransaction().rollback();
